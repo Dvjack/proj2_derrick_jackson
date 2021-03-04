f3881ef9821c36d5275ce840be65c0edf39cc7f8
Derrick <dvjack@umd.edu>
2021-03-04 14:47:04 -0500

    added the BFS algorithm

--- proj2.py
+++ proj2.py
@@ -2,62 +2,104 @@
 
 import numpy as np
 import cv2
+import queue as q
+import os
+
+obstacle = np.uint8(255)
 
 x = 200
 y = 100
 # generating and diplaying the trial map
-def trial():
+def get_trial():
     trial_map = np.zeros([y,x],np.uint8)
     trial_map[y-1-60:y-40-1,90:110] = 255
     # print(trial_map)
-    cv2.circle(trial_map,(160-1,y-50-1),15,(255,0,0),-1)
+    cv2.circle(trial_map,(160-1,y-50-1),15,255,-1)
     # print(trial_map[50][160])
     # cv2.imshow('map', trial_map)
     # cv2.waitKey(0)
+    return trial_map
 
-trial()
+trial_map = get_trial()
 
 def get_pos():
     start_col = int(input("Starting Column: "))
     start_row = int(input("Starting Row: "))
     starting_position = [start_col,start_row]
+    
     goal_col = int(input("Goal Column: "))
     goal_row = int(input("Goal Row: "))
     goal_position = [goal_col,goal_row]
 
-    print(f'start: {starting_position}')
-    print(f'goal: {goal_position}')
+    # print(f'start: {starting_position}')
+    # print(f'goal: {goal_position}')
+    return starting_position, goal_position
 
-get_pos()
+pos = get_pos()
+start = pos[0]
+goal = pos[1]
+print(f'start: {start}')
+print(f'goal: {goal}')
+
+parent_visited = [0]
+visited = [start]
+parent_q = q.Queue()
+parent_q.put_nowait(start)
 
 
-def Action_Move(start_col, start_row, direction):
+def Action_Move(start_col, start_row, parent_pos, direction):
 
     next_pos = (start_col + direction[0], start_row + direction[1])
+    print()
+    if next_pos[1] < x and next_pos[1] >= 0 and next_pos[0] < y and next_pos[0] >= 0 and trial_map[next_pos[0], next_pos[1]] != obstacle and next_pos not in visited:
 
-    if next_pos[0] < x and next_pos[0] >= 0 and next_pos[1] < y and next_pos[1] >= 0:
+        
+        visited.append(next_pos)
+        parent_visited.append(parent_pos)
+        parent_q.put_nowait(next_pos)
 
-        return
+        if next_pos[0] == goal[0] and next_pos[1] == goal[1]:
+            print("Goal has been reached")
+            print(f'End is: {next_pos}')
+            return True
+    return False
+
+
+my_list = []
     
-# try:
-#         goal_reached = False
-#         i =0
-#         while not goal_reached:
+try:
+        goal_reached = False
+        i =0
+        while not goal_reached:
 
+            print(f'I is:{i}')
+            print(list(parent_q.queue))
+            parent_pos = parent_q.get_nowait()
+            my_list.append(parent_pos)
 
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (0,-1))
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (0,-1))
            
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (0,1)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (0,1)) or goal_reached
             
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (-1,0)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (-1,0)) or goal_reached
             
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (1,0)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (1,0)) or goal_reached
 
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (-1,-1)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (-1,-1)) or goal_reached
 
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (1,-1)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (1,-1)) or goal_reached
 
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (-1,1)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (-1,1)) or goal_reached
 
-#             goal_reached = Action_Move(blank_tile_col,blank_tile_row, parent_state, (1,1)) or goal_reached
+            goal_reached = Action_Move(parent_pos[0],parent_pos[1], parent_pos, (1,1)) or goal_reached
 
+            if parent_q.empty():
+                print("No Solution")
+
+                break
+            i += 1
+except KeyboardInterrupt:
+    exit()
+
+                
+    
